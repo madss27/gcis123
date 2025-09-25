b5f862c787274d84c93d747c4a08a500705d0520
madss27 <mt8390@rit.edu>
2025-09-25 08:59:11 +0400

    mes

---
+++ bueh.py
@@ -0,0 +1,5 @@
+a="s 11 1 2 12 "
+t=a.split(" ")
+print(t[1])
+for i in t:
+    print(i)
---
+++ try.py
@@ -0,0 +1,6 @@
+#program to print 5 random numbers from 1 to 10
+import random           
+for i in range(5):
+    print(random.randint(1,10))
+
+#end of program
